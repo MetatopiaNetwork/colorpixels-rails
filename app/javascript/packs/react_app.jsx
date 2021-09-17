ccdd75290bafa937322dc28ce886e79876c6570b
Philippe Girard <philippe.girard5@gmail.com>
2021-09-17 15:42:55 -0400

    react frontend

--- app/javascript/packs/react_app.jsx
+++ app/javascript/packs/react_app.jsx
@@ -1,9 +1,22 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const Hello = props => (
+    <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+    name: 'David'
+}
+
+Hello.propTypes = {
+    name: PropTypes.string
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
-        <></>,
+        <Hello name="React" />,
         document.getElementById("react-app")
     )
 })
