02c72eadbf04683730903a05a02c729bf95fd172
Philippe Girard <philippe.girard5@gmail.com>
2021-09-20 12:14:29 -0400

    clip NFT button

--- app/javascript/react_src/pages/WatchPage.jsx
+++ app/javascript/react_src/pages/WatchPage.jsx
@@ -2,6 +2,10 @@ import React from "react"
 import {getLiveId} from "../bridge";
 import 'shaka-player/dist/controls.css';
 import VideoJSPlayer from "../components/players/VideoJSPlayer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Dropdown } from 'react-bootstrap';
+
+
 
 function WatchPage() {
 
@@ -21,6 +25,20 @@ function WatchPage() {
                 maxWidth: "700px",
             }}>
                 <VideoJSPlayer src="https://cdn.livepeer.com/hls/274ecn88f1pocum0/index.m3u8"/>
+
+                <div className="dropdown" style={{ textAlign: "right", marginTop: "20px"}}>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                            Clip NFT
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="">Last 10 seconds</Dropdown.Item>
+                            <Dropdown.Item href="">Last 20 seconds</Dropdown.Item>
+                            <Dropdown.Item href="">Last 30 seconds</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
             </div>
 
 
