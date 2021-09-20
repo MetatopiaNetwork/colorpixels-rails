7210e14c7ef3cac4b3daa1c7c266bcfc948c75b5
Philippe Girard <philippe.girard5@gmail.com>
2021-09-20 10:15:12 -0400

    styling

--- app/javascript/react_src/pages/WatchPage.jsx
+++ app/javascript/react_src/pages/WatchPage.jsx
@@ -8,7 +8,11 @@ function WatchPage() {
             <h1 style={{marginBottom: "0px"}}>
                 <i>EventName</i> Live Stream
             </h1>
-            <div style={{color: "grey", fontSize: "13px", marginBottom: "20px"}}>Live id: <i>{getLiveId()}</i></div>
+            <div style={{
+                color: "grey",
+                fontSize: "13px",
+                marginBottom: "20px"
+            }}>Live id: <i>{getLiveId()}</i></div>
             <div style={{
                 backgroundColor: "black",
                 width: "700px",
