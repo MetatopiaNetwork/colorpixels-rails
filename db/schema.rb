bca40afc01e833778a57f2396819c8b48a97ff32
Philippe Girard <philippe.girard5@gmail.com>
2021-09-22 15:14:57 -0400

    create clip table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_190340) do
+ActiveRecord::Schema.define(version: 2021_09_22_190923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_09_22_190340) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "clips", force: :cascade do |t|
+    t.bigint "events_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["events_id"], name: "index_clips_on_events_id"
   end
 
   create_table "creators", force: :cascade do |t|
