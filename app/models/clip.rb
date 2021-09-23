e696ef3f8c568cad4988a8e94c951bd14ca07424
Philippe Girard <philippe.girard5@gmail.com>
2021-09-23 17:03:10 -0400

    service url

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -5,9 +5,10 @@ class Clip < ApplicationRecord
 
   def video_url
     begin
-      return Rails.application.routes.url_helpers.rails_blob_path(video, only_path: true)
+      # return Rails.application.routes.url_helpers.rails_blob_path(video, only_path: true)
       # url_for(clip.video) # for redirect links
       # rails_blob_path(clip.video, disposition: "attachment") # for downloads links
+      return video.url
     rescue
       nil
     end
